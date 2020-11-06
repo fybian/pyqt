f863e7d945367460bb1b3c561c793011d12714a6
fybian <44440095+fybian@users.noreply.github.com>
2020-11-06 22:12:45 +0800

    add file path

--- word2pdf/main.py
+++ word2pdf/main.py
@@ -2,12 +2,14 @@ import sys
 import os
 import re
 from word2pdf import Ui_MainWindow
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+import PyQt5.QtGui as qg
 from win32com.client import gencache
 from win32com.client import constants, gencache
 
 count = 0
 num_file = 0
+directory = ''
 
 
 class trans(QMainWindow, Ui_MainWindow):  # 继承自父类QtWidgets.QMainWindow
@@ -17,6 +19,13 @@ class trans(QMainWindow, Ui_MainWindow):  # 继承自父类QtWidgets.QMainWindow
         self.setupUi(self)
         self.word2pdfButton.clicked.connect(self.word2pdfButton_clicked)
         self.excel2pdfButton.clicked.connect(self.excel2pdfButton_clicked)
+        self.openFileButton.clicked.connect(self.openFileButton_clicked)
+
+    def openFileButton_clicked(self):
+        global directory
+        directory = QFileDialog.getExistingDirectory(self, '选择文件夹', './')
+        self.FilePathText.setPlainText(directory)
+        directory = directory.replace('/', '\\')
 
     # word转pdf
     def word2pdf(self, wordPath, pdfPath):
@@ -24,6 +33,7 @@ class trans(QMainWindow, Ui_MainWindow):  # 继承自父类QtWidgets.QMainWindow
         doc = word.Documents.Open(wordPath, ReadOnly=1)
         doc.ExportAsFixedFormat(pdfPath,
                                 constants.wdExportFormatPDF,
+                                # OptimizeFor=constants.wdExportOptimizeForPrint,
                                 Item=constants.wdExportDocumentWithMarkup,
                                 CreateBookmarks=constants.wdExportCreateHeadingBookmarks)
         word.Quit(constants.wdDoNotSaveChanges)
@@ -32,9 +42,11 @@ class trans(QMainWindow, Ui_MainWindow):  # 继承自父类QtWidgets.QMainWindow
     def excel2pdf(self, excelPath, pdfPath):
         xlApp = gencache.EnsureDispatch('Excel.Application')
         xls = xlApp.Workbooks.Open(excelPath, ReadOnly=1)
-        xls.ExportAsFixedFormat(0,  pdfPath)
+        xls.ExportAsFixedFormat(
+            0, pdfPath, Quality=constants.xlQualityStandard, IgnorePrintAreas=False)
         xlApp.Quit()
 
+    # word转pdf按钮事件
     def word2pdfButton_clicked(self):
         self.word2pdfButton.setEnabled(False)
         self.excel2pdfButton.setEnabled(False)
@@ -42,20 +54,21 @@ class trans(QMainWindow, Ui_MainWindow):  # 继承自父类QtWidgets.QMainWindow
         self.textBrowser.setPlainText('开始转换')
         QApplication.processEvents()
 
-        for dirs, subdirs, files in os.walk(os.getcwd()):
+        for dirs, subdirs, files in os.walk(directory):
             for name in files:
                 global num_file
                 if re.search('\.(doc|docx)', name):
                     num_file += 1
         self.textBrowser.append(("共 %d 个Word文件" % (num_file)))
 
-        for dirs, subdirs, files in os.walk(os.getcwd()):
+        for dirs, subdirs, files in os.walk(directory):
             for name in files:
                 if re.search('\.(doc|docx)', name):
                     global count
                     count = count + 1
                     self.textBrowser.append(
                         ("第 %d / %d 个文件..." % (count, num_file)))
+                    self.textBrowser.append(name)
                     self.textBrowser.moveCursor(
                         self.textBrowser.textCursor().End)  # 文本框显示到底部
                     QApplication.processEvents()
@@ -67,25 +80,29 @@ class trans(QMainWindow, Ui_MainWindow):  # 继承自父类QtWidgets.QMainWindow
         self.word2pdfButton.setEnabled(True)
         self.excel2pdfButton.setEnabled(True)
 
+    # excel转pdf按钮事件
     def excel2pdfButton_clicked(self):
         self.word2pdfButton.setEnabled(False)
         self.excel2pdfButton.setEnabled(False)
+
         self.textBrowser.setPlainText('开始转换')
         QApplication.processEvents()
-        for dirs, subdirs, files in os.walk(os.getcwd()):
+
+        for dirs, subdirs, files in os.walk(directory):
             for name in files:
                 global num_file
                 if re.search('\.(xlsx|xls)', name):
                     num_file += 1
         self.textBrowser.append(("共 %d 个Excel文件" % (num_file)))
 
-        for dirs, subdirs, files in os.walk(os.getcwd()):
+        for dirs, subdirs, files in os.walk(directory):
             for name in files:
                 if re.search('\.(xlsx|xls)', name):
                     global count
                     count = count + 1
                     self.textBrowser.append(
                         ("第 %d / %d 个文件..." % (count, num_file)))
+                    self.textBrowser.append(name)
                     self.textBrowser.moveCursor(
                         self.textBrowser.textCursor().End)  # 文本框显示到底部
                     QApplication.processEvents()
